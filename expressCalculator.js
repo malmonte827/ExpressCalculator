fb2b38018ef66659302e9f1852a17da0f00b7bdd
Michael <malmonte454@proton.me>
2024-09-24 15:02:32 -0400

    Final Revision

--- expressCalculator.js
+++ expressCalculator.js
@@ -1,3 +1,88 @@
-const express = require('express');
-const ExpressError = require('expressError');
-const app = express()
+const express = require("express");
+const ExpressError = require("expressError");
+const app = express();
+const {
+    findMode,
+    findMean,
+    findMedian,
+    convertToNums,
+} = require("./helperFunctions");
+
+app.get("mean", function (req, res, next) {
+    if (!req.query.nums) {
+        throw new ExpressError("You must pass a query of nums", 400);
+    }
+
+    let convertToString = req.query.nums.split(",");
+
+    let nums = convertToNums(convertToString);
+    if (nums instanceof Error) {
+        throw new ExpressError(nums.message);
+    }
+
+    let result = {
+        operation: "mean",
+        result: findMean(nums),
+    };
+    return res.send(result);
+});
+
+app.get("median", function (req, res, next) {
+    if (!req.query.nums) {
+        throw new ExpressError("You must pass a query of nums", 400);
+    }
+
+    let convertToString = req.query.nums.split(",");
+
+    let nums = convertToNums(convertToString);
+    if (nums instanceof Error) {
+        throw new ExpressError(nums.message);
+    }
+
+    let result = {
+        operation: "median",
+        result: findMedian(nums),
+    };
+    return res.send(result);
+});
+
+app.get("mode", function (req, res, next) {
+    if (!req.query.nums) {
+        throw new ExpressError("You must pass a query of nums", 400);
+    }
+
+    let convertToString = req.query.nums.split(",");
+
+    let nums = convertToNums(convertToString);
+    if (nums instanceof Error) {
+        throw new ExpressError(nums.message);
+    }
+
+    let result = {
+        operation: "mode",
+        result: findMode(nums),
+    };
+    return res.send(result);
+});
+
+app.listen(3000, function(){
+    console.log(`Server starting on port 3000`)
+})
+
+app.use(function (req, res, next) {
+    const err = new ExpressError("Not Found",404);
+  
+    // pass the error to the next piece of middleware
+    return next(err);
+  });
+  
+  /** general error handler */
+  
+  app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+  
+    return res.json({
+      error: err,
+      message: err.message
+    });
+  });
--- helperFunctions.js
+++ helperFunctions.js
@@ -1,46 +1,72 @@
-function convertToNums(numsAsString){
-    let convertednums = []
+function convertToNums(numsAsString) {
+    let convertednums = [];
 
-    for(let i = 0; i < numsAsString.length; i++){
-        const num = Number(numsAsString[i])
+    for (let i = 0; i < numsAsString.length; i++) {
+        const num = Number(numsAsString[i]);
 
-        if (Number.isNaN(num)){
+        if (Number.isNaN(num)) {
             return new Error(
                 `The Value ${numsAsString[i]} is not a valid number`
-            )
+            );
         }
-    convertednums.push(num)
+        convertednums.push(num);
     }
-    return convertednums
+    return convertednums;
 }
 
-function createFrequancyCounter(){
-    return
+function createFrequancyCounter(arr) {
+    let count = {};
+    for (let num of arr) {
+        if (count[num]) {
+            count[num] += 1;
+        } else {
+            count[num] = 1;
+        }
+    }
+    return count;
 }
 
-function findMedian(nums){
+function findMedian(nums) {
+    nums.sort();
 
-    nums.sort()
-
-    let middleIndex = Math.floor(nums.length / 2)
+    let middleIndex = Math.floor(nums.length / 2);
     let median;
 
-    if(nums.length % 2 !== 0){
-        median = nums[middleIndex]
+    if (nums.length % 2 !== 0) {
+        median = nums[middleIndex];
     } else {
-       median = (nums[middleIndex]+nums[middleIndex - 1]) / 2
+        median = (nums[middleIndex] + nums[middleIndex - 1]) / 2;
     }
-    return median
+    return median;
 }
 
-function findMean(nums){
-    let sum = 0
-for(let i = 0; i < nums.length; i++){
-    sum += nums[i]
-}
-return sum/nums.length
+function findMean(nums) {
+    let sum = 0;
+    for (let i = 0; i < nums.length; i++) {
+        sum += nums[i];
+    }
+    return sum / nums.length;
 }
 
-function findMode(nums){
-    
+function findMode(arr) {
+    let freqCounter = createFrequancyCounter(arr);
+
+    let count = 0;
+    let mode;
+
+    for (let key in freqCounter){
+        if (freqCounter[key]>count){
+            mode = key
+            count = freqCounter[key]
+        }
+    }
+    return mode
 }
+
+module.exports = {
+    findMode,
+    findMean,
+    findMedian,
+    convertToNums,
+    createFrequancyCounter,
+};
